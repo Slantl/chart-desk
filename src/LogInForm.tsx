88d1ff4d864395e6c732a880db9088ad5e6a6e4f
Slantl <35684789+Slantl@users.noreply.github.com>
2022-12-25 13:53:15 +0600

    push commit

---
+++ src/LogInForm.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+
+interface Props {
+    loging: string,
+    setLoging: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const LogInForm: FC<Props> = ({ loging, setLoging }) => {
+    const [active, setActive] = useState(true)
+    const activateLog = () => {
+        setActive(true)
+    }
+    const activateSign = () => {
+        setActive(false)
+    }
+    
+    const exit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        setLoging("hidden")
+    }
+
+    const submit = () => {
+        if (loging == "hidden") {
+
+        } else {
+
+        }
+    }
+    return (
+        <div className={"bg-back bg-opacity-50 w-screen z-50 flex justify-center items-center h-screen " + loging}>
+            <form className="flex w-auto h-auto p-2 rounded-md flex-col bg-primary3 animate-scaleIn">
+                <div className="flex justify-between text-2xl mb-2 h-10">
+                    <div className={"cursor-pointer h-fit " + (active ? "sign-log" : "")} onClick={activateLog}>
+                        <p className="text-center">Log in</p>
+                    </div>
+                    <div className={"cursor-pointer h-fit " + (active ? "" : "sign-log")} onClick={activateSign}>
+                        <p className="text-center">Sign in</p>
+                    </div>
+                </div>
+                <div className="flex flex-col w-48 mx-4">
+                    <label htmlFor="" className="pl-2">Mail</label>
+                    <input type="email" className="input rounded-md" />
+                    <label htmlFor="" className="pl-2">Pass</label>
+                    <input type="password" className="input rounded-md" />
+                </div>
+                <div className="flex justify-between text-xl mt-2 mx-4">
+                    <button className="bg-accent3 rounded-md px-1" type="submit" onClick={submit}>Submit</button>
+                    <button className="bg-accent2 rounded-md px-1" onClick={exit}>Exit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
